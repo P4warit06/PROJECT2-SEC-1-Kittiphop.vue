ebcd458148658bf2ae8f03c14e7b6fd97c9dd417
P4warit <142296227+P4warit06@users.noreply.github.com>
2025-02-28 19:03:18 +0700

    Demo Header Context&SetComponent Example

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,8 @@ import Header from "./components/Header.vue";
 
 <template>
   <!-- Demo Header -->
-  <div class="flex item-center justify-center">
-    <div> Demo Header Context</div>
+  <div class="flex item-center justify-center ">
+   
         <Header>
           <template #logo></template>
           <template #Maintitle> Main template</template>
