4bb23994575e51fdf1c9fb90590d2757ff18a738
KittiphopOhm <142304707+Kittiphop0hm@users.noreply.github.com>
2025-03-03 23:55:11 +0700

    009-[add] test action bid feature

---
+++ src/components/Action.vue
@@ -0,0 +1,56 @@
+<script setup>
+import { ref } from 'vue';
+const persons = ref([
+    {
+        id:1,
+        name:"Somchai"
+    },
+    {
+        id:2,
+        name:"Somhow"
+    },
+    {
+        id:3,
+        name:"Somyui"
+    }
+]);
+const bid = ref(0)
+const currentBid = ref(0)
+
+const actionBid = () => {
+    
+}
+
+const showCurrentBid = (count) => {
+    return currentBid.value = count;    
+}
+
+const cancelBid = () => {
+    return bid.value = 0
+}
+</script>
+
+<template>
+    <div class="flex flex-col justify-center items-center max-w-full max-h-80">
+        <div class="h-10 w-full bg-green-300 flex justify-center items-center">
+            Welcome to Action
+        </div>
+
+        <div class="flex flex-col justify-center items-center">
+            <div>
+                Item
+            </div>
+            <p>{{ currentBid }}</p>
+            <span>Bid: <input v-model="bid" type="number" class="border border-black"></span>
+            <p>{{ bid }}</p>
+            <div class="p-2 m-2">
+                <button @click="showCurrentBid(bid)" class="mx-2 px-4 py-2 bg-green-400 rounded-lg cursor-pointer">Confirm</button>
+                <button @click="cancelBid" class="mx-2 px-4 py-2 bg-gray-200 rounded-lg cursor-pointer">Cancel</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
--- src/components/Slider.vue
+++ src/components/Slider.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { ref } from 'vue';
 const count = ref(0);
-const texts = ref(["Hello JS", "Hello TS", "Hello Java", "Hello C#"])
+const texts = ref(["Hello JS", "Hello TS", "Hello Java", "Hello C#", "Hello Docker"])
 const top = () => {
     return count.value < texts.value.length - 1 ? count.value++ : count.value = 0;
 }
@@ -15,12 +15,17 @@ const down = () => {
 <template>
     <div class="h-52 bg-amber-100 flex justify-center items-center">
         <div class="flex justify-center items-center">
-            <button @click="top()" class="px-2 bg-green-400 rounded-lg cursor-pointer">+</button>
+            <button @click="top()" class="px-4 py-2 bg-green-400 rounded-lg cursor-pointer">+</button>
             <div class="p-5">
                 <h1>{{ texts[count] }}</h1>
             </div>
-            <button @click="down()" class="px-2 bg-red-400 rounded-lg cursor-pointer">-</button>
+            <button @click="down()" class="px-4 py-2 bg-red-400 rounded-lg cursor-pointer">-</button>
         </div>
+    </div>
 
+    <div>
+        <div>
+            
+        </div>
     </div>
 </template>
