490c93c23bdf5fc5a1a46655acc836801ae1ebc6
Pongsakorn Sinsomboonsuk <pongsakorn.sins@kmutt.ac.th>
2025-03-05 21:56:26 +0700

    062-[update]  Update code in App.vue to the latest version.

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,15 @@
 <script setup>
-import Header from "./components/Header.vue";
-import Sidebar from "./components/Sidebar.vue";
-import Footer from "./components/Footer.vue";
+import Header from "./components/common/Header.vue";
+import Footer from "./components/common/Footer.vue";
+import ProductManager from "./components/product/ProductManager.vue";
+import { useRouter } from 'vue-router';
 import { ref } from "vue";
-
 </script>
-
 <template>
-  <div class="min-h-screen flex flex-col">
-    <Header />
-      <main class="flex">
-      <Sidebar>
-        
-      </Sidebar>
-      <div class="flex-1 p-6 bg-gray-50">
-          <RouterView />
-      </div>
-      </main>
-    <Footer />
-  </div>
-</template>
 
+  <Header/>
+  <ProductManager />
+  <Footer/>
+
+</template>
 <style scoped></style>
