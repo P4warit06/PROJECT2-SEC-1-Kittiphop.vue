865e574fbdc7f3739636bfba6ea8ec224556d3f6
Pongsakorn Sinsomboonsuk <mu3mmm@hotmail.com>
2025-03-05 21:46:34 +0700

    062-[add] Retrieve products from db.json & [edit] downgrade previous merge

---
+++ src/components/product/ProductManager.vue
@@ -0,0 +1,24 @@
+<script setup>
+import data from "../../../data/db.json";
+import ProductList from "./ProductList.vue";
+import AddProduct from "./AddProduct.vue";
+
+import { ref } from "vue";
+
+const myProducts = ref(data.products);
+
+const addProduct = (product) => {
+  //validate
+  myProducts.value.push(product);
+};
+</script>
+
+<template>
+  <div>
+    <!-- 3. call handler function -->
+    <!-- <AddProduct @add-new-product="addProduct" /> -->
+    <ProductList :products="myProducts"></ProductList>
+  </div>
+</template>
+
+<style scoped></style>
