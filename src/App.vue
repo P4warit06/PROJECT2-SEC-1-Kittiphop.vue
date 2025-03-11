4a894e18285938d6deccfaf7c19969bed338178a
SMILLJAa <mu3mmm@hotmail.com>
2025-03-11 14:53:36 +0700

    062-[fix] แก้ไขเพิ่มโค้ดที่เป็นปัจจุบัน </Footer>#	modified:   src/App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,9 @@
 <script setup>
-import Header from "./components/common/Header.vue";
-import Footer from "./components/common/Footer.vue";
-import ProductManager from "./components/product/ProductManager.vue";
-import { useRouter } from 'vue-router';
-import { ref } from "vue";
+
 </script>
 <template>
 
-  <Header/>
-  <ProductManager />
-  <Footer/>
+<RouterView/>
 
 </template>
 <style scoped></style>
