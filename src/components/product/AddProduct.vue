60dea42e92d62ae5cef82c775c5b4173c87a03c0
KittiphopOhm <142304707+Kittiphop0hm@users.noreply.github.com>
2025-03-13 22:51:30 +0700

    009-[add] create addProduct form

--- src/components/product/AddProduct.vue
+++ src/components/product/AddProduct.vue
@@ -1,53 +1,92 @@
 <script setup>
 import { ref } from "vue";
-//1. register your custom event
-const emits = defineEmits(["addNewProduct"]);
+import { previewBinaryFile } from '../lib/utilities.js'
+
+const catagories = ref([
+  {name:"Accessories", value:"accessories"},
+  {name:"Audio", value:"audio"},
+  {name:"Electronics", value:"electronics"},
+  {name:"Wearables", value:"wearables"}
+])
+
+const status = ref([
+  {name: "Active", value:"active"},
+  {name: "Low Stock", value:"low stock"},
+  {name: "Out of Stock", value:"out of stock"}
+  ])
+
 const newProduct = ref({
   name: "",
+  catagory: "",
   price: 0,
+  status: "",
+  stock: 0,
+  image: "",
+  description: ""
 });
 
-const checkData = () => {
-  emits("addNewProduct", newProduct.value);
-};
+const imgFileName = ref("")
+
+const chooseBinaryImg = (event) => {
+  imgFileName.value = event.target.files[0].name
+  if (imgFileName.value.endsWith(".jpg") || imgFileName.value.endsWith(".jpeg") || imgFileName.value.endsWith(".png")) {
+      newProduct.value.image = previewBinaryFile(event.target.files[0])
+  }
+}
 </script>
 
 <template>
-  <div class="max-w-lg mt-10 p-6 bg-white shadow-md rounded-md">
-    <h2 class="text-2xl font-bold mb-6 text-center">Add Product</h2>
+  <div class="mb-5">
+    <div>
+      <label>Product name: </label>
+      <input v-model="newProduct.name" type="text" placeholder="Enter product name" class="border border-black rounded-lg">
+    </div>
 
     <div>
-      <div class="mb-4">
-        <label class="text-gray-700 font-medium mb-2">Product Name</label>
-        <input
-          v-model="newProduct.name"
-          type="text"
-          class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500"
-          placeholder="Enter product name"
-        />
-      </div>
-      <div class="mb-6">
-        <label class="text-gray-700 font-medium mb-2">Price</label>
-        <input
-          v-model.number="newProduct.price"
-          type="number"
-          class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-indigo-500"
-        />
-      </div>
-      <!-- 2.event trigger -->
-      <button
-        @click="$emit('addNewProduct', newProduct)"
-        class="w-full bg-indigo-500 text-white py-2 px-4 rounded-md hover:bg-indigo-600 transition-colors duration-300"
-      >
-        Add Product
-      </button>
-      <!-- 1.เรียกข้างบน -->
-      <button
-        @click="checkData"
-        class="w-full bg-indigo-500 text-white py-2 px-4 rounded-md hover:bg-indigo-600 transition-colors duration-300"
-      >
-        Add Product
-      </button>
+      <label>Stock: </label>
+      <input v-model="newProduct.stock" min="0" type="number" placeholder="Stock" class="border border-black rounded-lg">
+    </div>
+
+    <div>
+      <label>Price: </label>
+      <input v-model="newProduct.price" min="0" type="number" placeholder="Price" class="border border-black rounded-lg">
+    </div>
+
+    <div>
+      <label>Status: </label>
+      <select v-model="newProduct.status" class="border border-black rounded-lg">
+        <option class="placeholder" selected disabled value="">Chose one</option>
+        <option v-for="(st, index) in status" :key="index" :value="st.value">{{ st.name }}</option>
+      </select>
+    </div>
+
+    <div>
+      <label>Catagory: </label>
+      <select v-model="newProduct.catagory" class="border border-black rounded-lg">
+        <option class="placeholder" selected disabled value="">Chose one</option>
+        <option v-for="(catagory, index) in catagories" :key="index" :value="catagory.value">{{ catagory.name }}</option>
+      </select>
+    </div>
+
+    <div>
+      <label>Upload Image </label>
+      <input class="border border-black rounded-lg cursor-pointer" type="file" accept=".jpg, .png, .jpeg" @change="chooseBinaryImg">
+      <br>
+      <a :href="newProduct.image" target="_blank">
+        {{ newProduct.image }}
+        <img v-show="newProduct.image" :src="newProduct.image" alt="img" width="100" height="100">
+      </a>
+    </div>
+
+    <div class="flex justify-start items-center">
+      <label>Description</label>
+      <textarea v-model="newProduct.description" placeholder="Product description" class="border border-black rounded-lg cursor-pointer"></textarea>
     </div>
   </div>
 </template>
+
+<style scoped>
+select > .placeholder {
+  display: none;
+}
+</style>
--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -2,16 +2,15 @@
       import Product from "../components/product/ProductManager.vue";
       import Header from "../components/common/Header.vue";
       import Footer from "../components/common/Footer.vue";
-
+      import AddProduct from "@/components/product/AddProduct.vue";
 </script>
  
 <template>
 <div>
+  <AddProduct></AddProduct>
   <Header/>
-  
   <Product/>
   <Footer/>
-  
 </div>
 </template>
  
