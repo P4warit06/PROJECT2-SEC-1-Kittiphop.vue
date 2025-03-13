af3a6286bba411560368c5a3439d03c23d77f45a
P4warit <142296227+P4warit06@users.noreply.github.com>
2025-03-14 03:09:44 +0700

    059-[Add] UpdateProduct Component

--- src/components/product/ProductManager.vue
+++ src/components/product/ProductManager.vue
@@ -2,34 +2,61 @@
 import data from "../../../data/db.json";
 import ProductList from "./ProductList.vue";
 import AddProduct from "./AddProduct.vue";
+import UpdateProduct from "./updateProduct.vue"; 
+
 import { ref } from "vue";
 
 const myProducts = ref(data.products);
-const isAdding = ref(false)
+const isAdding = ref(false);
+const isUpdate = ref(false);
+
+
 const addProduct = (product) => {
   if (product) {
-    myProducts.value.push(product)
-    isAdding.value = false
+    myProducts.value.push(product);
+    isAdding.value = false;
   }
-}
+};
+// ice 
+// Function to update 
+const updateProduct = (id, newData) => {  
+  const index = myProducts.value.findIndex(item => item.id === id);
+
+  if (index === -1) {
+    throw new Error(`Cannot find the product with ID: ${id}`);
+  }
+
+  myProducts.value[index] = { ...myProducts.value[index], ...newData };
+  isUpdate.value = false; // Hide update form after updating
+};
+
+// ทำเผื่อไว้ได้ใช้งาน
+const cancelUpdate = () => {
+  isUpdate.value = false;
+};
+// ice 
 
 const cancelAdding = () => {
-  isAdding.value = false
-}
-
+  isAdding.value = false;
+};
 </script>
 
 <template>
   <div>
     <div class="w-full flex justify-center items-center my-4">
-      <button
-      v-show="!isAdding"
-      @click="isAdding = !isAdding"
-      class="bg-green-400 py-2 px-4 rounded-lg cursor-pointer hover:opacity-80">Add Product</button>
+      <button v-show="!isAdding" @click="isAdding = !isAdding"
+        class="bg-green-400 py-2 px-4 rounded-lg cursor-pointer hover:opacity-80">
+        Add Product
+      </button>
     </div>
-    <AddProduct v-show="isAdding" @add-new-product="addProduct" @cancel-adding="cancelAdding" :is-adding="isAdding"></AddProduct>
+    <AddProduct v-show="isAdding" @add-new-product="addProduct" @cancel-adding="cancelAdding" :is-adding="isAdding">
+    </AddProduct>
+    <!-- // Ice  -->
+    <!-- <button v-show="!isUpdate" @click="isUpdate = !isUpdate" class="bg-blue-400 py-3 px-4 rounded-lg">
+      <span class="text-white">Update Product</span>
+    </button>
+    <UpdateProduct v-show="isUpdate" @update-product="updateProduct"></UpdateProduct> -->
+    <!-- Ice  -->
     <ProductList :products="myProducts"></ProductList>
   </div>
 </template>
-
-<style scoped></style>
--- src/components/product/updateProduct.vue
+++ src/components/product/updateProduct.vue
@@ -1,11 +1,121 @@
 <script setup>
+import { ref, watch } from "vue";
+import { previewBinaryFile } from "../lib/utilities.js";
 
+const emit = defineEmits(["updateProduct", "cancelEditing"]);
+const props = defineProps({ product: Object });
+
+const categories = ref([
+    { name: "Accessories", value: "Accessories" },
+    { name: "Audio", value: "Audio" },
+    { name: "Electronics", value: "Electronics" },
+    { name: "Wearables", value: "Wearables" },
+]);
+
+const status = ref([
+    { name: "Active", value: "Active" },
+    { name: "Low Stock", value: "Low Stock" },
+    { name: "Out of Stock", value: "Out of Stock" },
+]);
+
+const editedProduct = ref({ ...props.product });
+
+watch(
+    () => props.product,
+    (newProduct) => {
+        editedProduct.value = { ...newProduct };
+    },
+    { deep: true }
+);
+
+const imgFileName = ref("");
+
+const chooseBinaryImg = (event) => {
+    imgFileName.value = event.target.files[0].name;
+    if (
+        imgFileName.value.endsWith(".jpg") ||
+        imgFileName.value.endsWith(".jpeg") ||
+        imgFileName.value.endsWith(".png")
+    ) {
+        editedProduct.value.image = previewBinaryFile(event.target.files[0]);
+    }
+};
 </script>
-<template>
-    <div> 
-        <div class="blue">   </div>
-    </div>
- 
 
+<template>
+    <div class="p-6 bg-gray-100 rounded-lg shadow-lg max-w-3xl mx-auto">
+        <h1 class="text-xl  mb-1 text-center">Update Product</h1>
+        <div class="space-y-1.5">
+            <div>
+                <label class="block text-sm font-medium text-slate-700">Product Name:</label>
+                <input v-model="editedProduct.name" type="text"
+                    class="w-full border border-gray-300 rounded-lg px-3 py-2 focus:ring-2 focus:ring-sky-700 text-emerald-400" />
+            </div>
+
+            <div>
+                <label class="block text-sm font-medium mb-1">Stock:</label>
+                <input v-model="editedProduct.stock" type="number" min="0"
+                    class="w-full border border-gray-300 rounded-lg px-3 py-2 focus:ring-2 focus:ring-sky-700 text-emerald-400" />
+            </div>
+
+            <div>
+                <label class="block text-sm font-medium mb-1">Price:</label>
+                <input v-model="editedProduct.price" type="number" min="0"
+                    class="w-full border border-gray-300 rounded-lg px-3 py-2 focus:ring-2 focus:ring-sky-700 text-emerald-400" />
+            </div>
+
+            <div>
+                <label class="block text-sm font-medium mb-1">Status:</label>
+                <select v-model="editedProduct.status"
+                    class="w-full border border-gray-300 rounded-lg px-3 py-2 focus:ring-2 focus:ring-sky-700 text-emerald-400">
+                    <option class="placeholder" disabled value="">Choose one</option>
+                    <option v-for="(st, index) in status" :key="index" :value="st.value">{{ st.name }}</option>
+                </select>
+            </div>
+
+            <div>
+                <label class="block text-sm font-medium mb-1">Category:</label>
+                <select v-model="editedProduct.category"
+                    class="w-full border border-gray-300 rounded-lg px-3 py-2 focus:ring-2 focus:ring-sky-700 text-emerald-400">
+                    <option class="placeholder" disabled value="">Choose one</option>
+                    <option v-for="(category, index) in categories" :key="index" :value="category.value">{{
+                        category.name }}</option>
+                </select>
+            </div>
+
+            <div>
+                <label class="block text-sm font-medium mb-1">Upload Image:</label>
+                <input
+                    class="w-full border border-gray-300 rounded-lg px-3 py-2 cursor-pointer focus:ring-2 focus:ring-sky-700 shadow-md"
+                    type="file" accept=".jpg, .png, .jpeg" @change="chooseBinaryImg" />
+                <div class="mt-3">
+                    <a :href="editedProduct.image" target="_blank" class="text-blue-500 underline">{{
+                        editedProduct.image }}</a>
+                    <img v-show="editedProduct.image" :src="editedProduct.image" alt="img"
+                        class="mt-2 rounded-lg border w-24 h-24 object-cover">
+                </div>
+            </div>
+
+            <div>
+                <label class="block text-sm font-medium mb-1">Description:</label>
+                <textarea v-model="editedProduct.description"
+                    class="w-full border border-gray-300 rounded-lg px-3 py-2 focus:ring-2 focus:ring-sky-700 text-emerald-400"></textarea>
+            </div>
+
+            <div class="w-full flex flex-col justify-center items-center">
+                <div class="w-full flex justify-center items-center mb-4">
+                    <button @click="$emit('updateProduct', editedProduct)"
+                        class="w-full bg-blue-400 py-2 px-4 rounded-lg cursor-pointer hover:opacity-80 text-white">Update
+                        Product</button>
+                </div>
+
+            </div>
+        </div>
+    </div>
 </template>
-<style scoped></style>
+
+<style scoped>
+select>.placeholder {
+    display: none;
+}
+</style>
