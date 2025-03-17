8f564fa7e0e42590f36345374c21ab71362596fe
P4warit <142296227+P4warit06@users.noreply.github.com>
2025-03-17 21:00:04 +0700

    059-[Add]Demo Login page
    add demo login vue

---
+++ src/components/page/Login.vue
@@ -0,0 +1,36 @@
+<script setup>
+i
+</script>
+
+<template>
+  <div class="relative min-h-screen">
+    <!-- Background Image -->
+    <div class="absolute inset-0 z-0"/>
+      
+    
+    <!-- Navigation Bar -->
+    <div class="relative z-10">
+      <Navbar/>
+      
+      <!-- Main Content -->
+      <MainContent>
+        <h1 class="text-white text-4xl md:text-5xl lg:text-6xl font-bold mb-4">
+          LOGIN 
+        </h1>
+        
+      </MainContent>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+/* Additional custom styles if needed */
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@700&display=swap');
+
+h1, h2, button, a {
+  font-family: 'Montserrat', sans-serif;
+  text-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+}
+
+
+</style>
