822c6952abdc5cb32acb99983e57e6218dd8629f
P4warit <142296227+P4warit06@users.noreply.github.com>
2025-03-17 20:57:24 +0700

    059-[Add] Router update products
    Add Router path of update product

--- src/router/index.js
+++ src/router/index.js
@@ -1,34 +1,40 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import AboutView from '../views/AboutView.vue'
-import ProductView from '../views/ProductView.vue'
-import ProductDetailView from '../views/ProductDetailView.vue'
-
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import AboutView from "../views/AboutView.vue";
+import ProductView from "../views/ProductView.vue";
+import ProductDetailView from "../views/ProductDetailView.vue";
+import updateProduct from "@/components/product/updateProduct.vue";
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
+      path: "/",
+      name: "home",
       component: HomeView,
     },
     {
-      path: '/about',
-      name: 'about',
+      path: "/about",
+      name: "about",
       component: AboutView,
     },
     {
-      path: '/products',
-      name: 'products',
+      path: "/products",
+      name: "products",
       component: ProductView,
     },
     {
-      path: '/products/:id',
-      name: 'productDetail',
+      path: "/products/:id",
+      name: "productDetail",
       component: ProductDetailView,
-      props: true
+      props: true,
+    },
+    {
+      path: "/products/update/:productId",
+      name: "updateProduct",
+      component: updateProduct,
+      props: true,
     },
   ],
-})
+});
 
-export default router
+export default router;
