32c5358367882cb5e64799d0532d67cd7af24b3f
KittiphopOhm <142304707+Kittiphop0hm@users.noreply.github.com>
2025-03-21 01:23:15 +0700

    009-[update] add to cart

--- src/components/UserProductManager.vue
+++ src/components/UserProductManager.vue
@@ -2,7 +2,7 @@
 import UserProductList from './UserProductList.vue';
 import Header from "./Header.vue";
 import { ref, onMounted } from 'vue'
-import { getItems, getItemById, editItem } from '../libs/fetchUtils.js'
+import { getItems, getItemById, editItem, addItem } from '../libs/fetchUtils.js'
 
 const myProducts = ref([])
 const myCarts = ref([])
@@ -19,8 +19,14 @@ const currentProduct = ref({})
 const addProductToCart = async (product) => {
     try {
         currentProduct.value = await getItemById(`${import.meta.env.VITE_APP_URL}/products`, product.id)
-        console.log(currentProduct.value);
         
+        if (currentProduct.value && currentProduct.value.stock > 0) {
+            currentProduct.value.stock =- 1
+            const addCart = await addItem(`${import.meta.env.VITE_APP_URL}/carts`, currentProduct.value)
+            if (addCart) {
+              myCarts.value.push(addCart)
+            }
+        }       
     } catch(error) {
         console.log(error);
     }
