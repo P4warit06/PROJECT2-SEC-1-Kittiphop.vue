0550838e74cd981829c83808825724bc52da1cf7
KittiphopOhm <142304707+Kittiphop0hm@users.noreply.github.com>
2025-03-24 21:27:08 +0700

    fix ui

--- src/components/UserCartList.vue
+++ src/components/UserCartList.vue
@@ -110,13 +110,13 @@ const inputQuantity = async (item) => {
                 </div>
                 <div class="flex justify-center items-center space-x-2">
                     <div>
-                        <button @click="addQuantity(yourProduct)" class="px-3 py-1 bg-green-500 text-white rounded-lg hover:bg-green-600 transition duration-300 cursor-pointer">+</button>
+                        <button @click="subtractQuantity(yourProduct)" class="px-3 py-1 bg-gray-500 text-white rounded-lg hover:bg-gray-600 transition duration-300 cursor-pointer">-</button>
                     </div>
                     <div>
                         <input @input="inputQuantity(yourProduct)" type="number" v-model="yourProduct.quantity" min="0" :max="yourProduct.stock" class="w-16 border border-gray-300 rounded-lg px-3 py-1 text-center text-xl font-semibold bg-white">
                     </div>
                     <div>
-                        <button @click="subtractQuantity(yourProduct)" class="px-3 py-1 bg-gray-500 text-white rounded-lg hover:bg-gray-600 transition duration-300 cursor-pointer">-</button>
+                        <button @click="addQuantity(yourProduct)" class="px-3 py-1 bg-green-500 text-white rounded-lg hover:bg-green-600 transition duration-300 cursor-pointer">+</button>
                     </div>
                 </div>
                 <div class="flex items-center space-x-2">
