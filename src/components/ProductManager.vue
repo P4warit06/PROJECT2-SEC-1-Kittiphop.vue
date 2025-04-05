3166e390195a6a5c399bbc8f39e828cebb3d62ea
P4warit <142296227+P4warit06@users.noreply.github.com>
2025-04-05 17:10:59 +0700

    059-[Update] Decorate Button UI

--- src/components/ProductManager.vue
+++ src/components/ProductManager.vue
@@ -212,35 +212,19 @@ const cancelAdd = () => {
 <template>
   <div class="p">
     <HeaderManager />
-    <FilterProduct
-      :categories="filterCategories"
-      :status="filterStatus"
-      @filter-product="filterProduct"
-    />
+    <FilterProduct :categories="filterCategories" :status="filterStatus" @filter-product="filterProduct" />
     <ProductTracker />
-    <button
-      @click="isAdding = !isAdding"
-      class="text-green-600 hover:text-green-400 underline cursor-pointer"
-    >
+    <button @click="isAdding = !isAdding"
+      class="px-4 py-2 bg-gradient-to-r from-green-600 to-green-300 text-white font-semibold rounded-2xl shadow-md hover:from-green-500 hover:to-green-300 transition duration-300 ease-in-out transform hover:scale-105">
       Add New Product
     </button>
 
-    <AddEditProduct
-      v-if="isAdding || isEditing"
-      :active-product="currentProduct"
-      @add-new-product="addNewProduct"
-      @edit-product="updateProduct"
-      @cancel-adding="cancelAdd"
-    />
 
-    <ProductList
-      v-show="!isAdding && !isEditing"
-      @deleteProduct="deleteExistProduct"
-      @setEditing="setEditProduct"
-      :products="productForFilter"
-      :selectedProducts="selectedProducts"
-      @selectDeleteProduct="deleteMultipleProduct"
-    />
+    <AddEditProduct v-if="isAdding || isEditing" :active-product="currentProduct" @add-new-product="addNewProduct"
+      @edit-product="updateProduct" @cancel-adding="cancelAdd" />
+
+    <ProductList v-show="!isAdding && !isEditing" @deleteProduct="deleteExistProduct" @setEditing="setEditProduct"
+      :products="productForFilter" :selectedProducts="selectedProducts" @selectDeleteProduct="deleteMultipleProduct" />
   </div>
 </template>
 
