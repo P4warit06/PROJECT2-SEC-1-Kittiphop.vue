c8af2edf36d9993df672154f04e10c877512c024
P4warit <142296227+P4warit06@users.noreply.github.com>
2025-04-09 17:47:57 +0700

    059-[Update] Fix Product Tracker

--- src/components/ProductTracker.vue
+++ src/components/ProductTracker.vue
@@ -91,7 +91,7 @@ onUnmounted(() => {
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                         d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
                 </svg>
-                <input v-model="trackingId" type="text" placeholder="PSLU 8002936"
+                <input v-model="trackingId" type="text" placeholder="Please Enter Id 1-8 ..."
                     class="bg-transparent w-full outline-none text-gray-700 placeholder-gray-500 text-base"
                     @keyup.enter="startTracking" />
             </div>
